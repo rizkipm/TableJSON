30b5589eb42774aee58d795a858c600fb7521d6b
Programmer Muslim <rizki@imastudio.co.id>
2017-11-08 23:45:18 +0700

    add alamofire

---
+++ TableJSON/Pods/Target Support Files/Pods-TableJSON/Pods-TableJSON-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TableJSON : NSObject
+@end
+@implementation PodsDummy_Pods_TableJSON
+@end
---
+++ TableJSON/TableJSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableJSON
+//
+//  Created by Rizki Syaputra on 11/8/17.
+//  Copyright © 2017 Rizki Syaputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
